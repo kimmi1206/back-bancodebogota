2e151b4cfaf66b20ec9d19819de87a2893609586
kimmi1206 <kheinerimm1206@gmail.com>
2024-12-13 15:35:10 -0500

    Added SSL on port 8443 and HTTP on port 8080
    Signed-off-by: kimmi1206 <kheinerimm1206@gmail.com>

--- src/main/java/prueba/bancodebogota/backend/config/TomcatConfig.java
+++ src/main/java/prueba/bancodebogota/backend/config/TomcatConfig.java
@@ -1,4 +1,21 @@
 package prueba.bancodebogota.backend.config;
 
-public class TomcatConfig {
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.web.embedded.tomcat.*;
+import org.springframework.boot.web.server.*;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class TomcatConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    @Value("${server.http.port}")
+    private int httpPort;
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(httpPort);
+        factory.addAdditionalTomcatConnectors(connector);
+    }
 }
